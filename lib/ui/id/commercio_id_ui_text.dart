048f4bae3ca3b1c86844fd7edf0b68bd8b4587e9
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-06-25 16:27:46 +0200

    Add DeriveDidDocumentText

--- lib/ui/id/commercio_id_ui_text.dart
+++ lib/ui/id/commercio_id_ui_text.dart
@@ -95,7 +95,7 @@ class RestoreKeysText extends CommercioTextWidget<
         );
 }
 
-class DeriveDidDocumentText extends CommercioTextWidget<
+class DeleteKeysText extends CommercioTextWidget<
     CommercioIdDeleteKeysBloc,
     CommercioIdDeleteKeysEvent,
     CommercioIdDeletedKeysState,
@@ -103,7 +103,7 @@ class DeriveDidDocumentText extends CommercioTextWidget<
     CommercioIdDeletedKeysStateData,
     CommercioIdDeletedKeysStateLoading,
     CommercioIdDeletedKeysStateError> {
-  const DeriveDidDocumentText({
+  const DeleteKeysText({
     Key key,
     TextStyle style,
     TextStyle loadingStyle,
@@ -120,6 +120,53 @@ class DeriveDidDocumentText extends CommercioTextWidget<
     ui.TextHeightBehavior textHeightBehavior,
     @required
         String Function(CommercioIdDeletedKeysStateData state) textCallback,
+    @required String Function() loadingTextCallback,
+  }) : super(
+          key: key,
+          style: style,
+          loadingStyle: loadingStyle,
+          strutStyle: strutStyle,
+          textAlign: textAlign,
+          textDirection: textDirection,
+          locale: locale,
+          softWrap: softWrap,
+          overflow: overflow,
+          textScaleFactor: textScaleFactor,
+          maxLines: maxLines,
+          semanticsLabel: semanticsLabel,
+          textWidthBasis: textWidthBasis,
+          textHeightBehavior: textHeightBehavior,
+          textCallback: textCallback,
+          loadingTextCallback: loadingTextCallback,
+        );
+}
+
+class DeriveDidDocumentText extends CommercioTextWidget<
+    CommercioIdDeriveDidDocumentBloc,
+    CommercioIdDeriveDidDocumentEvent,
+    CommercioIdDerivedDidDocumentState,
+    CommercioIdDerivedDidDocumentStateInitial,
+    CommercioIdDerivedDidDocumentStateData,
+    CommercioIdDerivedDidDocumentStateLoading,
+    CommercioIdDerivedDidDocumentStateError> {
+  const DeriveDidDocumentText({
+    Key key,
+    TextStyle style,
+    TextStyle loadingStyle,
+    StrutStyle strutStyle,
+    TextAlign textAlign,
+    TextDirection textDirection,
+    Locale locale,
+    bool softWrap,
+    TextOverflow overflow,
+    double textScaleFactor,
+    int maxLines,
+    String semanticsLabel,
+    TextWidthBasis textWidthBasis,
+    ui.TextHeightBehavior textHeightBehavior,
+    @required
+        String Function(CommercioIdDerivedDidDocumentStateData state)
+            textCallback,
     @required String Function() loadingTextCallback,
   }) : super(
           key: key,
