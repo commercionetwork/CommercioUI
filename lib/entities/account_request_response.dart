9b35304de567b8b32dd55e8afbf56850dcb991f2
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-07-01 14:37:57 +0200

    Change toString to make more informative

--- lib/entities/account_request_response.dart
+++ lib/entities/account_request_response.dart
@@ -11,7 +11,7 @@ class AccountRequestResponse {
   });
 
   @override
-  String toString() => message;
+  String toString() => '{ message: $message, isSuccess: $isSuccess }';
 }
 
 /// Represents an HTTP response with error.
