c58f1134a73b03a6e028a1dcf3177c6fd7af0008
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-07-01 14:30:19 +0200

    Rename freezedEquals to hasType, stricter comparing

--- lib/core/utils/type_helper.dart
+++ lib/core/utils/type_helper.dart
@@ -1,5 +1,9 @@
 class TypeHelper {
-  static bool freezedEquals<A, B>(A a, B b) {
-    return a.runtimeType.toString().contains(b.toString());
+  /// Returns [true] if the object [a] has type [b].
+  ///
+  /// [a] should be an instance (e.g. an object) of class [A], whether [b]
+  /// should be a [Type].
+  static bool hasType<A, B>(A a, B b) {
+    return a.runtimeType.toString().replaceAll('_\$', '') == b.toString();
   }
 }
